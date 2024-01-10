d62fe2b4124e1c45a317acc9054289d47f2a131f
Victoria <v.kurgina@yahoo.com>
2024-01-10 11:41:18 +0300

    Update Api Service(fixed bag)

--- APIService/index.js
+++ APIService/index.js
@@ -7,19 +7,29 @@ const booksData = require('./data/books.json');
 
 //создаем приложения express
 const app = express();
-
 // применяем cors
 app.use(cors());
 
-app.get('/random-book', function (req, res) {
-  const randomIdx = Math.floor(Math.random() * booksData.length);
-  const randomBook = booksData[randomIdx];
-  //возвращаем клиенту одну книгу
+// function getRandomBook() {
+//   const randomIndex = Math.floor(Math.random() * booksData.length);
+//   const randomBook = booksData[randomIndex];
+//   return randomBook;
+// }
+
+app.get('/random-book', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * booksData.length);
+  const randomBook = booksData[randomIndex];
   res.json(randomBook);
 });
 
+// app.get('/random-book-delayed', (req, res) => {
+//   setTimeout(() => {
+//     res.json(getRandomBook());
+//   }, 2000);
+// });
+
 //запускаем сервер на определенном порту
 const port = process.env.PORT || 4000;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
